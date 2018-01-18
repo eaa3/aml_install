8acb411accb0f9f6896db14c8a44403c7a09cb10
Ermano Arruda <exa371@bham.ac.uk>
2018-01-18 00:53:26 +0000

    fixing local run of install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 INSTALL_TYPE=$0
+WORKSPACE_PATH=$1
 
+echo "INSTALL_TYPE: $INSTALL_TYPE"
+echo "WORKSPACE_PATH: $WORKSPACE_PATH"
+
+# checking if this script is being run from the curl-based install script or locally from host machine
+# parameter base is different if run locally
+if [ "$INSTALL_TYPE" == "./install.sh" ]
+then
+	INSTALL_TYPE=$1
+	WORKSPACE_PATH=$2
+fi
 
 if [ -z "$INSTALL_TYPE" ] || [ "$INSTALL_TYPE" == "bash" ]
 then 
@@ -14,4 +25,4 @@ fi
 rm -rf /tmp/aml_install
 git clone --depth 1 -b master https://github.com/eaa3/aml_install.git /tmp/aml_install
 cd /tmp/aml_install
-./install_${INSTALL_TYPE}.sh $1
+./install_${INSTALL_TYPE}.sh $WORKSPACE_PATH
